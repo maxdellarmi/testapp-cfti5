cc351e6372f995c26d0dd64cbc26206baad769d2
maxdellarmi <max.dellarmi@gmail.com>
2023-02-02 17:40:20 +0100

    Update replaceIndexEECacheSED.sh

--- storage/app/public/replaceIndexEECacheSED.sh
+++ storage/app/public/replaceIndexEECacheSED.sh
@@ -1,1 +1,1 @@
-sed -i 's=http:\\/\\/localhost=https:\\/\\/93.64.166.202:8003=g' IndexEEdataFullCache.json
+sed -i 's=http:\\/\\/localhost=https:\\/\\/93.64.166.202:8003=g' IndexEEdataFullCached.json
